4fa4eed7c1be29d3d85c3865a63e16a5684b459f
caaiomf <caiofavarao@hotmail.com>
2025-10-25 21:06:06 -0300

    inclusao da tabuada

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ server.get('/', (requisicao, resposta) => {
     </head>
     <body>
       <h2>Nova Aplicação</h2>
-      <p><a href="/horaAtual">Ver hora atual</a></p>
     </body>
     </html>
   `);
@@ -40,10 +39,60 @@ server.get('/horaAtual', (requisicao, resposta) => {
     </head>
     <body>
       <h1>Agora são ${hora}</h1>
-      <p><a href="/">Voltar</a></p>
     </body>
     </html>
   `);
+});
+
+//criar um metodo que aceita parametro
+server.get('/tabuada', (requisicao, resposta) =>{
+    //tabuada de qual numero e até qual sequencia? 
+    const numero = requisicao.query.numero;
+    const sequencia = requisicao.query.sequencia;
+    if(!numero || !sequencia) {
+        resposta.send(`<!DOCTYPE html>
+                    <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>tabuada</title>
+                </head>
+                <body>
+                    <h1>Tabuada</h1>
+                    <h2>Por favor, Informe o número e a sequencia na URL</h2>
+                    <h3>Exemplo: http://localhost:3000/tabuada?numero=5&sequencia=10</h3>
+                    <p><a href="/">Voltar</a></p>
+                </body>
+                </html>
+            `);
+    } else{
+        resposta.write(`<!DOCTYPE html>
+                    <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>tabuada</title>
+                </head>
+                <body>
+                    <h1>Tabuada do ${numero} até a sequencia ${sequencia}</h1>
+                    <ul>
+            `);
+            for( let i=0; i<=sequencia ;i++){
+                resposta.write(`<li>${i} x ${numero} = ${i*numero}</li>`);
+            }
+
+            resposta.write(`
+                    </ul>
+                </body>
+                </html>
+                `);
+        resposta.end(); // finaliza e envia
+    }
+
+    console.log("requisição tabuada");
+
 });
 
 // Iniciar servidor
